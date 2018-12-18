0e2d3be4c24857cdbbee24e717a8627ec982e6fc
smazher <syed.mazher@psquickit.com>
2018-12-18 18:13:47 +0530

    UI Fixes

--- RollbarApplication/Global.asax.cs
+++ RollbarApplication/Global.asax.cs
@@ -9,6 +9,8 @@ using System.Web.Routing;
 using SharpRaven;
 using RollBar.App_Start;
 using RollbarApplication.App_Start;
+using Rollbar.Telemetry;
+using Rollbar.DTOs;
 
 namespace RollbarApplication
 {
@@ -26,11 +28,30 @@ namespace RollbarApplication
             {
                 Environment = "production"
             });
-
+            ConfigureRollbarTelemetry();
+            ConfigureRollbarServer();
             //var ravenClient = new RavenClient("https://83b1b8aeded04c109de4f0e11a5be07d@sentry.io/1354823");
            
             //GlobalFilters.Filters.Add(new HandleErrorAttribute());
             //GlobalFilters.Filters.Add(new RavenHandleErrorAttribute());
+        }
+
+        private static void ConfigureRollbarTelemetry()
+        {
+            TelemetryConfig telemetryConfig = new TelemetryConfig(
+              telemetryEnabled: true,
+              telemetryQueueDepth: 3
+            );
+            TelemetryCollector.Instance.Config.Reconfigure(telemetryConfig);
+        }
+
+        private static void ConfigureRollbarServer()
+        {
+            Server server = new Server();
+           // server.CodeVersion = "00e9466";
+            server.Root = "/WebApplication2";
+            server.Branch = "master";
+            RollbarLocator.RollbarInstance.Config.Server = server;
         }
     }
 }
